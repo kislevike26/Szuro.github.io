4abb8815aac97082202d761901f7081f23882ff6
kislevike26 <szurcsi@gmail.com>
2026-05-05 09:55:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const textElement = document.getElementById('typing-text');
-    // A neved helyett most ezt írja ki középen
-    const title = "SZAKMAI PORTFÓLIÓ"; 
-    let i = 0;
+function switchTab(id, btn) {
+    document.querySelectorAll('.list-container').forEach(l => l.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
 
-    function type() {
-        if (i < title.length) {
-            textElement.innerHTML += title.charAt(i);
-            i++;
-            setTimeout(type, 120);
-        }
-    }
-    
-    setTimeout(type, 800);
-});
+    document.querySelectorAll('.menu-btn').forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    document.getElementById('tab-title').innerText = btn.innerText;
+}
